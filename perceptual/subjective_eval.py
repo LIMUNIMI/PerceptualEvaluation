c5458288881f949f9c7a47ffd72d7927144c47e9
Federico Simonetta <federico.simonetta@unimi.it>
2020-07-21 19:23:41 +0200

    Added rescaling, error margin, violin plots, better pvalue

--- perceptual/subjective_eval.py
+++ perceptual/subjective_eval.py
@@ -11,6 +11,7 @@ import dash_html_components as html
 import numpy as np
 import xml.etree.ElementTree as ET
 from tqdm import tqdm
+from joblib import Parallel, delayed
 
 PATH = "/home/sapo/Develop/http/listening/saves"
 DISCARD_BEFORE_THAN = datetime.datetime(year=2020,
@@ -19,7 +20,7 @@ DISCARD_BEFORE_THAN = datetime.datetime(year=2020,
                                         hour=12,
                                         minute=45,
                                         second=0)
-MAP_VALUES = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4}
+MAP_VALUES = {'0': 0, '1': 0, '2': 1, '3': 1, '4': 1}
 
 METHODS = {
     '0': 'hidden reference',
@@ -179,7 +180,11 @@ VALUES (
     return db
 
 
-def sqlite2pandas(db, variable=None, min_listen_time=5, cursor_moved=True):
+def sqlite2pandas(db,
+                  variable=None,
+                  ordinal=False,
+                  min_listen_time=5,
+                  cursor_moved=True):
     """
     Given an sqlite db, this does a query and returns the table in a dataframe
     """
@@ -190,7 +195,7 @@ def sqlite2pandas(db, variable=None, min_listen_time=5, cursor_moved=True):
     else:
         SQL = 'SELECT '
 
-    SQL += '"question", "excerpt_num", "method", "rating"\
+    SQL += '"question", "excerpt_num", "method", "rating", "user_id"\
         FROM ANSWERS JOIN USERS ON user_id == USERS.id'
 
     need_where = True
@@ -213,7 +218,23 @@ def sqlite2pandas(db, variable=None, min_listen_time=5, cursor_moved=True):
         SQL += f'"listen_time" >= {min_listen_time}'
 
     print("Extracting data")
-    return pd.read_sql(SQL, db)
+    data = pd.read_sql(SQL, db)
+    if ordinal:
+        # changing ratings according to the ordinal value
+        questions = data['question'].unique()
+        excerpts = data['excerpt_num'].unique()
+        users = data['user_id'].unique()
+        for question in questions:
+            d0 = data.loc[data['question'] == question]
+            for excerpt in excerpts:
+                d1 = d0.loc[d0['excerpt_num'] == excerpt]
+                for user in users:
+                    d2 = d1.loc[d1['user_id'] == user]
+                    arr = d2['rating'].to_numpy()
+                    sort_idx = np.argsort(arr)
+                    arr[sort_idx] = list(range(len(arr)))
+                    data.loc[d2.index, 'rating'] = arr
+    return data
 
 
 def count_users(db):
@@ -236,7 +257,12 @@ def count_users(db):
     print("\n")
 
 
-def plot(path, excerpts_mean=True, variable=None, *args, **kwargs):
+def plot(path,
+         excerpts_mean=True,
+         variable=None,
+         ordinal=False,
+         *args,
+         **kwargs):
     """
     Arguments
     ---------
@@ -251,6 +277,9 @@ def plot(path, excerpts_mean=True, variable=None, *args, **kwargs):
         the variable to be observed: each value of the variable will be a
         different line and the Wilcoxon rank test will be computed for all the
         combinations of the variable value
+
+    `ordinal` : bool
+        if ratings should be considered by ordinal or not
 
     `*args, **kwargs` :
         other arguments passed to `sqlite2pandas` :
@@ -270,71 +299,11 @@ def plot(path, excerpts_mean=True, variable=None, *args, **kwargs):
     `list[dash_core_components.Graph]` :
         the list of graphs to be plotted in Dash
     """
-    def _plot_data(selcted_data):
-        groupby = [variable, 'method']
-        if not variable:
-            groupby = 'method'
-        data = selected_data.groupby(
-            groupby, as_index=True)['rating'].mean().to_frame()
-        data['std'] = selected_data.groupby(groupby,
-                                            as_index=True)['rating'].std()
-        data['count'] = selected_data.groupby(groupby,
-                                              as_index=True)['rating'].count()
-        data = data.reset_index()
-        fig_plot = px.bar(
-            data,
-            x='method',
-            y='rating',
-            color=variable,
-            barmode='group',
-            text='count',
-            title=f'question {question}, excerpt {excerpt}, variable {variable}',
-            error_y='std')
 
-        # fig_plot.update_traces(textposition='outside')
-        fig_plot.write_image(
-            os.path.join(SAVE_PATH,
-                         f'plot-{question}_{excerpt}_{variable}.svg'))
-
-        # computing wilcoxon matrix
-        if variable:
-            variables = data[variable].unique()
-            pval = np.ones((len(variables), len(variables)))
-            for i, expi in enumerate(variables):
-                for j, expj in enumerate(variables):
-                    if i != j:
-                        try:
-                            _, pval[i, j] = scipy.stats.wilcoxon(
-                                data.loc[data[variable] == expi]['rating'],
-                                data.loc[data[variable] == expj]['rating'])
-                        except Exception as e:
-                            print("\nError in Wilcoxon test!:")
-                            print(e)
-                            print()
-            fig_pval = px.imshow(
-                pval,
-                x=variables,
-                y=variables,
-                title=f'question {question}, excerpt {excerpt}, variable {variable}',
-                range_color=[0, 0.5])
-
-            fig_pval.write_image(
-                os.path.join(SAVE_PATH,
-                             f'pval-{question}_{excerpt}_{variable}.svg'))
-            return html.Div([
-                html.Div([dcc.Graph(figure=fig_plot)], className="col-md-8"),
-                html.Div([dcc.Graph(figure=fig_pval)], className="col-md-4")
-            ],
-                className="row")
-
-        else:
-            return dcc.Graph(figure=fig_plot)
-
-    graphs = []
     # loading data from xml to pandas
     db = xml2sqlite(path)
     count_users(db)
-    df = sqlite2pandas(db, variable=variable, *args, **kwargs)
+    df = sqlite2pandas(db, variable=variable, ordinal=ordinal, *args, **kwargs)
 
     # taking the number of questions and excerpts
     questions = df.question.unique()
@@ -342,20 +311,149 @@ def plot(path, excerpts_mean=True, variable=None, *args, **kwargs):
 
     # selecting data
     print("Plotting")
-    for question in tqdm(questions):
+
+    def process(question):
+        graphs = []
         if excerpts_mean:
             excerpt = 'mean'
-            selected_data = df.loc[df['question'] == question]
+            selected_data = df.loc[df['question'] == question].sort_values(
+                ['method', variable])
             # plotting means of the excerpts
-            graphs.append(_plot_data(selected_data))
+            graphs.append(
+                _plot_data(selected_data, question, excerpt, variable))
         else:
             for excerpt in sorted(excerpts):
                 selected_data = df.loc[df['question'] == question].loc[
-                    df['excerpt_num'] == excerpt]
+                    df['excerpt_num'] == excerpt].sort_values(['method', variable])
                 # plotting each excerpt
-                graphs.append(_plot_data(selected_data))
+                graphs.append(
+                    _plot_data(selected_data, question, excerpt, variable))
+        return graphs
 
-    return graphs
+    graphs = Parallel(n_jobs=-2)(delayed(process)(question)
+                                 for question in tqdm(questions))
+
+    return [g0 for g1 in graphs for g0 in g1]
+
+
+def _plot_data(selected_data, question, excerpt, variable):
+    fig_plot = px.violin(
+        selected_data,
+        x='method',
+        y='rating',
+        box=True,
+        points="all",
+        color=variable,
+        violinmode='group',
+        title=f'question {question}, excerpt {excerpt}, variable {variable}')
+
+    fig_plot.update_traces(meanline_visible=True)
+
+    # fig_plot.write_image(
+    #     os.path.join(SAVE_PATH, f'plot-{question}_{excerpt}_{variable}.svg'))
+
+    if variable:
+        variables = selected_data[variable].unique()
+        methods = selected_data['method'].unique()
+
+        fig_pvals = _compute_wilcoxon_pvals(selected_data, question, excerpt,
+                                            variable)
+
+        text = [
+            "(size, margin error) with 95% of confidence",
+            html.Br(), "methods: "
+        ]
+        text += [str(method) + ", " for method in methods]
+        text.append(html.Br())
+        for var in variables:
+            text.append(f"var {var}: ")
+            for method in methods:
+                # computing std, expected sample size and real sample size:
+                samples = selected_data.loc[selected_data[variable] == var]
+                samples = samples.loc[samples['method'] == method]['rating']
+                sample_size = samples.count()
+                std = samples.std()
+                margin_error = np.sqrt((1.96**2) * (std**2) / sample_size)
+                text += [f"({sample_size} {margin_error:.2f}) "]
+            text.append(html.Br())
+        return html.Div([
+            html.Div([
+                html.Div([dcc.Graph(figure=fig_plot)], className="col-md-12"),
+            ],
+                className="row"),
+            html.Div([
+                html.Div([dcc.Graph(figure=fig)], className="col-md-2")
+                for fig in fig_pvals
+            ],
+                className="row"),
+            html.P(text)
+        ])
+
+    else:
+        return dcc.Graph(figure=fig_plot)
+
+
+def _compute_wilcoxon_pvals(selected_data, question, excerpt, variable):
+    variables = selected_data[variable].unique()
+    methods = selected_data['method'].unique()
+    # computing wilcoxon matries for each method
+    fig_pvals = []
+    for method in methods:
+        samples = selected_data.loc[selected_data['method'] == method]
+        pval = np.ones((len(variables), len(variables)))
+        for i, expi in enumerate(variables):
+            for j, expj in enumerate(variables):
+                if i != j:
+                    try:
+                        datai = samples.loc[samples[variable] == expi]
+                        dataj = samples.loc[samples[variable] == expj]
+                        maxlen = min(len(datai), len(dataj))
+                        _, pval[i, j] = scipy.stats.wilcoxon(
+                            datai['rating'][:maxlen], dataj['rating'][:maxlen])
+                    except Exception as e:
+                        print("\nError in Wilcoxon test!:")
+                        print(e)
+                        print()
+        fig_pval = px.imshow(pval,
+                             x=variables,
+                             y=variables,
+                             title=f'method {method}',
+                             range_color=[0, 0.5])
+
+        # fig_pval.write_image(
+        #     os.path.join(SAVE_PATH,
+        #                  f'pval-{question}_{excerpt}_{variable}.svg'))
+
+        fig_pvals.append(fig_pval)
+
+    # computing wilcoxon matries for each variable
+    for var in variables:
+        samples = selected_data.loc[selected_data[variable] == var]
+        pval = np.ones((len(methods), len(methods)))
+        for i, expi in enumerate(methods):
+            for j, expj in enumerate(methods):
+                if i != j:
+                    try:
+                        datai = samples.loc[samples['method'] == expi]
+                        dataj = samples.loc[samples['method'] == expj]
+                        maxlen = min(len(datai), len(dataj))
+                        _, pval[i, j] = scipy.stats.wilcoxon(
+                            datai['rating'][:maxlen], dataj['rating'][:maxlen])
+                    except Exception as e:
+                        print("\nError in Wilcoxon test!:")
+                        print(e)
+                        print()
+        fig_pval = px.imshow(pval,
+                             x=methods,
+                             y=methods,
+                             title=f'var {var}',
+                             range_color=[0, 0.5])
+
+        # fig_pval.write_image(
+        #     os.path.join(SAVE_PATH,
+        #                  f'pval-{question}_{excerpt}_{variable}.svg'))
+        fig_pvals.append(fig_pval)
+    return fig_pvals
 
 
 if __name__ == "__main__":
@@ -364,12 +462,13 @@ if __name__ == "__main__":
         print(f"""
 Syntax:
 
-{sys.argv[0]} [variable] [true|false]
+{sys.argv[0]} [variable] [true|false] [ordinal]
 
 * `variable`: one of 'expertise', 'headphones', 'habits_classical', 'habits_general'. Default: 'expertise'
 
 * `true|false`: if true, the average over the excerpts are computed, if false, the results for each single excerpts are shown. Default: true
 
+* `ordinal` : if used, absolute ratings given by users are substituted with their their ordinal position (higher is more similar)
 """)
         variable = 'expertise'
     else:
@@ -381,16 +480,18 @@ Syntax:
             excerpt_mean = True
     else:
         excerpt_mean = True
+
+    ordinal = False
+    if len(sys.argv) > 3:
+        if sys.argv[3] == 'ordinal':
+            ordinal = True
+
     graphs = plot(PATH,
                   variable=variable,
                   excerpts_mean=excerpt_mean,
                   min_listen_time=1,
-                  cursor_moved=False)
-    # graphs = plot(PATH,
-    #               variable='expertise',
-    #               excerpts_mean=True,
-    #               min_listen_time=5,
-    #               cursor_moved=True)
+                  cursor_moved=False,
+                  ordinal=ordinal)
 
     app = dash.Dash(external_stylesheets=[
         'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'
